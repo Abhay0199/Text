48e210392bac5ad10bf0869656bee8d6f0ca5ac2
Abhay099 <abhay199901@gmail.com>
2023-08-28 12:52:08 +0530

    files Added

---
+++ app.js
@@ -0,0 +1,17 @@
+let express = require("express");
+let app = express();
+app.use(express.json());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GEt, POST , OPTIONS, PUT, PATCH,DELETE,HEAD"
+    );
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin,X-Requested-With,Content-Type,Accept"
+    );
+    next();
+});
+const port = 2410;
+app.listen(port, () => console.log(`Node app listening on port ${port}!`));
